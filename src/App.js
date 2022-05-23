002dd2c9a916057d2c14f50eb002ae60ae6e8a5b
Jhakailash <kailashjhaji7@gmail.com>
2022-05-23 15:37:11 +0530

    reactexam1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AddTask } from "./components/AddTask";
+import { Counter } from "./components/Counter";
+import { Task } from "./components/Task";
+import { TaskHeader } from "./components/TaskHeader";
+import { Tasks } from "./components/Tasks";
+
+function App() {
+  return <div>{/* Code Here */}</div>;
+}
+
+export default App;
